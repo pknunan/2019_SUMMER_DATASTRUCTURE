67d60b725e3118bd343118d3cfc5e840ebe21573
Ga-Yeon Park <rkdusdd52@naver.com>
2019-07-09 03:04:05 +0900

    Rename 1-3.cpp to 2-3.cpp

---
+++ 2주차 과제/박가연/1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+#define CAPACITY 100
+#define BUFFER_SIZE 20
+
+char* words[CAPACITY];
+int numbers[CAPACITY] = { 0 }, n = 0;
+
+int search(char *buf);
+int main(void)
+{
+	char buf[30];
+	FILE *fp = fopen("harry.txt", "r");
+	FILE *fp2 = fopen("words.txt", "w");
+	while (fscanf(fp, "%s", buf) != EOF)
+	{
+		if(strlen(buf)<6)
+			continue;
+		else if (search(buf) != -1)
+		{
+			numbers[search(buf)]++;
+			continue;
+		}
+		else
+		{
+			int i = n - 1;
+			while (i >= 0 && strcmp(words[i], buf) > 0)
+			{
+				words[i + 1] = words[i];
+				numbers[i + 1] = numbers[i];
+				i--;
+			}
+			words[i + 1] = strdup(buf);
+			numbers[i + 1] = 0;
+		}
+		n++;
+	}
+	for (int i = 0; i < n; i++)
+		fprintf(fp2, "%s %d\n", words[i], numbers[i]+1);
+	return 0;
+}
+int search(char *buf)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (*words[i] == *buf)
+			return i;
+	}
+	return -1;
+}
---
+++ 2주차 과제/박가연/2.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#define CAPACITY 100
+#define BUFFER_SIZE 20
+
+char* words[CAPACITY];
+int n = 0;
+void check(char answer[], char guess[], char c);
+int main(void)
+{
+	char buf[BUFFER_SIZE];
+	FILE *fp = fopen("harry.txt", "r");
+	while (fscanf(fp, "%s", buf) != EOF)
+	{
+		if (strlen(buf) >= 6)
+		{
+			words[n] = strdup(buf);
+			n++;
+		}
+	}
+	while (1)
+	{
+		srand((unsigned)time(NULL));
+		int j = rand() % n;
+		char c, guess[CAPACITY], answer[CAPACITY];
+		strcpy(answer, words[j]);
+		int length = strlen(answer);
+		for (int i = 0; i < length; i++)
+		{
+			guess[i] = '*';
+			printf("%c", guess[i]);
+		}	
+		printf("\n");
+		char k;
+		while (1)
+		{
+			if (strncmp(answer,guess,length)==0)
+			{
+				printf("Try again(y/n)?");
+				scanf(" %c", &k);
+				if (k == 'y')
+					break;
+				else if (k == 'n')
+					return 0;
+			}
+			printf("Guess an alphabet : ");
+			char c;
+			scanf(" %c", &c);
+			check(answer,guess,c);
+			for (int i = 0; i<length; i++)
+				printf("%c", guess[i]);
+			printf("\n");
+		}
+	}
+	return 0;
+}
+void check(char answer[], char guess[], char c)
+{
+	for (int i = 0; i < strlen(answer); i++)
+	{
+		if (answer[i] == c)
+			guess[i] = c;
+	}
+}
